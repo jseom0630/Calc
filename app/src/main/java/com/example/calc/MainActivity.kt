6a5ea0b7e1068daf3a642ce35750476aa74b4dc1
jaesik <jseom0630@gmail.com>
2021-11-19 10:31:56 +0900

    20211119

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -15,8 +15,8 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    println()
 
     }
 }
